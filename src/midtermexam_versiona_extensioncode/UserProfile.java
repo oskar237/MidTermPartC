12bcbd68da289ecaacc6b3e3595d66e1288e4bed
oskar237 <158367210+oskar237@users.noreply.github.com>
2024-02-21 14:40:19 -0500

    Update UserProfile.java
    // Added comments for demonstration purposes
    // Author: Oskar Cristic Lawrence

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -1,3 +1,7 @@
+// Added comments for demonstration purposes
+// Author: Oskar Cristic Lawrence
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
